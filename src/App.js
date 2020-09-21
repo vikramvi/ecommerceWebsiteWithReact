a778f02ce742f770c4b3bfbc364d7cb3d77b4ae0
Vikram Ingleshwar <vikram.ingleshwar@yahoo.com>
2020-09-21 17:37:57 +0530

    added child components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import './App.css';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome } from "@fortawesome/free-solid-svg-icons";
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
 
 
 function App() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-6">column number one</div>
-        <div className="col-6">
-          <span>
-            <i className="fas fa-home" />
-            <FontAwesomeIcon icon={faHome} />
-          </span>
-        </div>
-      </div>
-    </div>
+    <React.Fragment>
+      <Navbar></Navbar>
+      <ProductList></ProductList>
+      <Details></Details>
+      <Cart></Cart>
+      <Default></Default>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <h3>NavBar</h3>
+            </div>
+        );
+    }
+
+}
+
+export default Navbar;
