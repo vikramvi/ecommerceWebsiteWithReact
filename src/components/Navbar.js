9b0158c27c61b636ecf93ac40ba1949a4a55f0df
Vikram Ingleshwar <vikram.ingleshwar@yahoo.com>
2020-09-22 11:09:54 +0530

    added functionality to Navbar with react router

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,33 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../logo.svg';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
 
 class Navbar extends Component {
     render() {
         return (
-            <div>
-                <h3>NavBar</h3>
-            </div>
+            <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+                <Link to="/">
+                    <img src={logo} alt="store" className="navbar-brand"></img>
+                </Link>
+                <ul className="navbar-nav align-items-center">
+                    <li className="nav-item ml-5">
+                        <Link to="/" className="nav-link">
+                            products
+                        </Link>
+                    </li>
+                </ul>
+                <Link to="/cart" className="ml-auto">
+                    <button>
+                        <FontAwesomeIcon icon={faCartPlus} />
+                        My Cart
+                    </button>
+                </Link>
+            </nav>
         );
     }
-
 }
 
 export default Navbar;
