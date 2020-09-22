28cafe85f2d11192d7dbfd0c794d7f31cb638d3e
Vikram Ingleshwar <vikram.ingleshwar@yahoo.com>
2020-09-22 12:29:19 +0530

    added styled components to Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,12 @@ import logo from '../logo.svg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
 
+import styled from 'styled-components';
+
 class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
                 <Link to="/">
                     <img src={logo} alt="store" className="navbar-brand"></img>
                 </Link>
@@ -20,14 +22,45 @@ class Navbar extends Component {
                     </li>
                 </ul>
                 <Link to="/cart" className="ml-auto">
-                    <button>
-                        <FontAwesomeIcon icon={faCartPlus} />
+                    <ButtonContainer>
+                        <span className="mr-2">
+                            <FontAwesomeIcon icon={faCartPlus} />
+                        </span>
                         My Cart
-                    </button>
+                    </ButtonContainer>
                 </Link>
-            </nav>
+            </NavWrapper>
         );
     }
 }
+
+const ButtonContainer = styled.button`
+text-transform: capitalize;
+font-size: 1.4 rem;
+background: transparent;
+border: 0.05rem solid var(--lightBlue);
+color: var(--lightBlue);
+border-radius: 0.5rem;
+padding: 0.2rem 0.5rem;
+cursor: pointer;
+margin: 0.2rem 0.5rem 0.2rem 0;
+transition: all 0.5s ease-in-out;
+&:hover{
+    background: var(--lightBlue);
+    color: var(--mainBlue);
+}
+&:focus{
+    outline: none;
+}
+`;
+
+const NavWrapper = styled.nav`
+    background: var(--mainBlue);
+    .nav-link{
+        color: var(--mainWhite) !important;
+        font-size: 1.3rem;
+        text-transform: capitalize; 
+    }
+`;
 
 export default Navbar;
