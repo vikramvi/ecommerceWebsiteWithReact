311c68c04edffede6d132ae1d8582c0907cf02b6
Vikram Ingleshwar <vikram.ingleshwar@yahoo.com>
2020-09-22 19:48:04 +0530

    added functionality to show all products' image

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../context";
 
 class Product extends Component {
     render() {
+        const { id, title, img, price, inCart } = this.props.product;
+
         return (
-            <div>
-                <h3>Product</h3>
-            </div>
+            <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+                <div className="card">
+                    <div className="img-container p-5" onClick={() => console.log("clicked product")}>
+                        <Link to="/details">
+                            <img src={img} alt="product image" className="card-img-top" />
+                        </Link>
+                    </div>
+                </div>
+            </ProductWrapper >
         )
     }
 }
+
+const ProductWrapper = styled.div`
+
+`;
 
 export default Product;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,17 +1,11 @@
 import React, { Component } from "react";
 import Product from "./Product";
 import Title from "./Title";
-import { storeProducts } from "../data";
 import { ProductConsumer } from "../context";
 
 class ProductList extends Component {
-    state = {
-        products: storeProducts
-    };
 
     render() {
-        //console.log(this.state.products);
-
         return (
             <React.Fragment>
                 <div className="py-5">
@@ -20,7 +14,9 @@ class ProductList extends Component {
                         <div className="row">
                             <ProductConsumer>
                                 {(value) => {
-                                    console.log(value);
+                                    return value.products.map(product => {
+                                        return <Product key={product.id} product={product} />
+                                    })
                                 }}
                             </ProductConsumer>
                         </div>
