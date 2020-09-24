539de21e8824af2e4e53cbb6797a69c31f1d3df1
Vikram Ingleshwar <vikram.ingleshwar@yahoo.com>
2020-09-24 18:23:19 +0530

    Empty cart child component functionality added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,34 @@
 import React, { Component } from "react";
+
+//child components
 import Title from '../Title';
 import CartColumns from './CartColumns';
+import EmptyCart from './EmptyCart';
+
+//context class
+import { ProductConsumer } from '../../context';
 
 class Cart extends Component {
     render() {
         return (
             <section>
-                <Title name="your" title="cart" />
-                <CartColumns />
+                <ProductConsumer>
+                    {value => {
+                        const { cart } = value;
+                        if (cart.length > 0) {
+                            return (
+                                <>
+                                    <Title name="your" title="cart" />
+                                    <CartColumns />
+                                </>
+                            )
+                        } else {
+                            return (
+                                <EmptyCart />
+                            )
+                        }
+                    }}
+                </ProductConsumer>
             </section>
         )
     }
