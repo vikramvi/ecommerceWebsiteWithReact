e8a0b1ba75313b233452732402680d82370d2cff
Vikram Ingleshwar <vikram.ingleshwar@yahoo.com>
2020-09-24 10:35:06 +0530

    code added to show correct detail page from product list

--- src/context.js
+++ src/context.js
@@ -30,13 +30,21 @@ class ProductProvider extends Component {
         });
     };
 
-    handleDetail = () => {
-        console.log("Hello from detail");
-    }
+    getItem = (id) => {
+        const product = this.state.products.find(item => item.id === id)
+        return product;
+    };
+
+    handleDetail = (id) => {
+        const product = this.getItem(id);
+        this.setState(() => {
+            return { detailProduct: product };
+        })
+    };
 
     addToCart = (id) => {
         console.log(`Hello from add to cart id is ${id}`);
-    }
+    };
 
     render() {
         return (
